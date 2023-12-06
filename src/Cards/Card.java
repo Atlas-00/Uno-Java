18122f98c07d1af7ace6bbb457afc3b770625e80
Atlas-00 <121752652+Atlas-00@users.noreply.github.com>
2023-12-06 16:57:06 -0300

    Ajout du dossier Cars et des ces classe

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -3,4 +3,12 @@ package Cards;
 public abstract class Card {
 	protected abstract String mName();
 	public abstract void playCard();
+	private final String color;
+
+	public Card(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
 }
